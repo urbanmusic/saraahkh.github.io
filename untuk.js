3d71047764bcc0b6b36ba1d524c2d6bf6068c5b2
saraahkh <118122426+saraahkh@users.noreply.github.com>
2022-11-14 16:13:03 +0700

    Add files via upload

---
+++ untuk.js
@@ -0,0 +1,18 @@
+alert("Welcome to Urban Music")
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
+}
+
